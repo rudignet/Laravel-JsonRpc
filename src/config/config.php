1d8340e244bfb17d0e46c3f5188c2d64fda0785c
Carlos Mora <ozonosp@gmail.com>
2015-01-12 19:08:48 +0100

    Doc

--- src/config/config.php
+++ src/config/config.php
@@ -5,7 +5,7 @@
       'methods' => array('POST'),  //Allowed http methods to call the server
       'resolvers' => array(
           'default' => '\{class}Controller', //ResolverName => resolverTemplate, {class} will be replaced by className (method is className.StaticFunctionName)
-          'sample' => '\TestController' //ResolverName => resolverTemplate, Sample with fix className, remote client can't choice the controller name
+          'sample' => '\TestController' //ResolverName => resolverTemplate, Sample with fix className, remote client can't choice the controller name.
       ),
       'allowed' => array(
           'localhost' => '', //Ip and key, for localhost ip must be localhost, key can be blank or null
